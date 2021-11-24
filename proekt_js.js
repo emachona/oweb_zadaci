9a5c00c2e41712e94dbc39a89a1dbbcea5f3174e
Emilija Chona <emmachona01@gmail.com>
2021-11-24 12:00:18 +0100

    navbar-js

--- proekt_js.js
+++ proekt_js.js
@@ -1,39 +1,83 @@
 var date=new Date();
-var hour=now.getHours();
+var hour=date.getHours();
+var minutes=date.getMinutes();
+var godina=date.getUTCFullYear();
+var mesec=date.getUTCMonth();
+var den=date.getDay();
 
 var username;
 username = window.prompt("Hi there! \n Внеси го твоето име:");
 
 document.getElementById("zdravo").innerHTML="Добредојде на мојата страна, " + username + "!";
 
+/*function toggleText(){
+    var x = document.getElementById("menu");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }*/
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropdown')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  const nav=document.querySelector(".menu");
+  let lastScrollY=window.scrollY;
+
+  window.addEventListener("scroll",() => {
+    if(lastScrollY< window.scrollY){
+      nav.classList.add("nav--hidden");
+    }
+    else{
+     nav.classList.remove("nav--hidden");
+    }
+    lastScrollY=window.scrollY;
+  });
+
 function getwords() {
-    var text = words.value;
-    document.getElementById("para").innerHTML += '<p>'+ username +" : "+ text;
-    document.getElementById("words").value = "коментар";
+    var text=document.getElementById("words").value;
+    document.getElementById("para").innerHTML +="<div>" + username + " : "+ text + "</div>";
+    document.getElementById("words").value = "Коментирај";
 }
 
 function getwords2() {
-    var text2 = words.value;
-    document.getElementById("para2").innerHTML += '<p>' + username +" : "+text2;
-    document.getElementById("words").value = "коментар";
+    var text2 = document.getElementById("words2").value;
+    document.getElementById("para2").innerHTML +="<div>" + username + " : " + text2 + "</div>";
+    document.getElementById("words2").value = "Коментирај";
 }
 
 function getwords3() {
-    var text3 = words.value;
-    document.getElementById("para3").innerHTML += '<p>'+ username +" : " +text3;
-    document.getElementById("words").value = "коментар";
+    var text3 = document.getElementById("words3").value;
+    document.getElementById("para3").innerHTML += "<div>" + username + " : " +text3+ "</div>"; 
+    document.getElementById("words3").value = "Коментирај";
 }
 
 function getwords4() {
-    var text4 = words.value;
-    document.getElementById("para4").innerHTML += '<p>'+ username +" : "+text4;
-    document.getElementById("words").value = "коментар";
+    var text4 = document.getElementById("words4").value;
+    document.getElementById("para4").innerHTML += "<div>"+ username +" : "+text4 + "</div>";
+    document.getElementById("words4").value = "Коментирај";
 }
 
 function getcomment() {
-    var comm = comment.value;
-    document.getElementById("komentar").innerHTML += '<p>'+ username +" : '"+comm +
-     "'\n - "+ date + hour;
-    document.getElementById("comment").value = "коментар";
+    var comm = document.getElementById("komentarprashaj").value;
+    document.getElementById("komentar").innerHTML += "<div>"+ "<strong>"+username+"</strong>"+" : " + comm +
+     "<br>"+ den+"-"+mesec+"-"+godina+" "+hour+":"+minutes +"<hr></div>";
+    document.getElementById("komentarprashaj").value = "";
 }
 
